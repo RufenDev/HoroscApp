a5c19454ab5323b6ae1d9dae2bc2dee7bbe2d6b5
RufenDev <rufen.dev@gmail.com>
2023-10-11 21:44:40 -0400

    commit v1.0.7

--- app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
@@ -1,5 +1,7 @@
 package com.example.horoscapp.ui.luck
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,15 +10,23 @@ import android.view.View.INVISIBLE
 import android.view.View.ROTATION
 import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.Animation.AnimationListener
 import android.view.animation.AnimationUtils
 import android.view.animation.DecelerateInterpolator
+import androidx.activity.OnBackPressedCallback
 import androidx.core.animation.doOnEnd
-import androidx.core.view.isVisible
+import androidx.core.animation.doOnStart
 import androidx.fragment.app.Fragment
 import com.example.horoscapp.R
+import com.example.horoscapp.R.anim.fade_in
+import com.example.horoscapp.R.anim.fade_out
+import com.example.horoscapp.R.anim.move_card_down
+import com.example.horoscapp.R.anim.move_card_up
+import com.example.horoscapp.R.anim.show_roulette
+import com.example.horoscapp.R.anim.hide_roulette
+import com.example.horoscapp.R.animator.flip_card_back
+import com.example.horoscapp.R.animator.flip_card_front
 import com.example.horoscapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlin.random.Random
@@ -26,6 +36,8 @@ class LuckFragment : Fragment() {
 
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+    private var itsAnimated = false
+    private var isPredictionShowing = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,7 +48,6 @@ class LuckFragment : Fragment() {
         _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -51,78 +62,177 @@ class LuckFragment : Fragment() {
     }
 
     private fun initListeners() {
-        binding.ivLuckyRoulette.setOnClickListener {
-            spinRoulette()
+        binding.ivLuckyRoulette.setOnClickListener { spinRoulette() }
+
+        binding.btnLuckTryAgain.setOnClickListener {
+            if (isPredictionShowing && !itsAnimated) {
+                itsAnimated = true
+                flipCard(false)
+            }
         }
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (!itsAnimated) {
+                    if (isPredictionShowing) {
+                        itsAnimated = true
+                        flipCard(false)
+
+                    } else {
+                        isEnabled = false
+                        requireActivity().onBackPressedDispatcher.onBackPressed()
+                        isEnabled = true
+                    }
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
     }
 
     private fun spinRoulette() {
-        val oneSpin = 360
-        val degrees = (Random.nextInt(oneSpin * 3) + oneSpin).toFloat()
-
-        val animator = ObjectAnimator.ofFloat(binding.ivLuckyRoulette, ROTATION, 0f, degrees)
-
-        animator.duration = 1000
-        animator.interpolator = DecelerateInterpolator()
-
-        animator.doOnEnd { slideCard() }
-        animator.start()
+        if (!itsAnimated) {
+            itsAnimated = true
+            val degrees = (Random.nextInt(360 * 2) + 90).toFloat()
+            val animator = ObjectAnimator.ofFloat(binding.ivLuckyRoulette, ROTATION, 0f, degrees)
+            animator.apply {
+                duration = 1200
+                interpolator = DecelerateInterpolator()
+            }
+            animator.doOnEnd {
+                moveCard(true)
+            }
+            animator.start()
+        }
     }
 
-    private fun slideCard() {
-        val slideUp = AnimationUtils.loadAnimation(context, R.anim.slide_up)
+    private fun moveCard(up: Boolean) {
+        val animation =
+            AnimationUtils.loadAnimation(context, if (up) move_card_up else move_card_down)
 
-        slideUp.setAnimationListener(object : AnimationListener {
+        animation.setAnimationListener(object : AnimationListener {
             override fun onAnimationRepeat(animation: Animation?) {}
-
             override fun onAnimationStart(animation: Animation?) {
+                if (up) {
+                    binding.ivLuckyCard.visibility = VISIBLE
+                }
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                if (up) {
+                    scaleCard(true)
+
+                } else {
+                    binding.ivLuckyCard.visibility = INVISIBLE
+                    itsAnimated = false
+                    isPredictionShowing = false
+                }
+            }
+        })
+
+        binding.ivLuckyCard.startAnimation(animation)
+    }
+
+    private fun scaleCard(grow: Boolean) {
+        val card = AnimationUtils.loadAnimation(context, if (grow) R.anim.grow else R.anim.shrink)
+
+        card.setAnimationListener(object : AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {}
+            override fun onAnimationStart(animation: Animation?) {
+                showRoulette(!grow)
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                if (grow) {
+                    flipCard(true)
+
+                } else {
+                    moveCard(false)
+                }
+            }
+        })
+        binding.ivLuckyCard.startAnimation(card)
+
+        val tarot = AnimationUtils.loadAnimation(context, if (grow) R.anim.grow else R.anim.shrink)
+        binding.ivLuckPredictionCard.startAnimation(tarot)
+    }
+
+    private fun showRoulette(show: Boolean) {
+        val animation =
+            AnimationUtils.loadAnimation(context, if (show) show_roulette else hide_roulette)
+
+        animation.setAnimationListener(object : AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {}
+            override fun onAnimationStart(animation: Animation?) {
+                if (show) {
+                    binding.ivLuckyRoulette.visibility = VISIBLE
+                }
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                if (!show) {
+                    binding.ivLuckyRoulette.visibility = INVISIBLE
+                }
+            }
+        })
+
+        binding.ivLuckyRoulette.startAnimation(animation)
+    }
+
+    private fun flipCard(show: Boolean) {
+        val front = AnimatorInflater.loadAnimator(context, flip_card_front) as AnimatorSet
+        val back = AnimatorInflater.loadAnimator(context, flip_card_back) as AnimatorSet
+
+        if(show){
+            front.setTarget(binding.ivLuckyCard)
+            back.setTarget(binding.ivLuckPredictionCard)
+
+        } else {
+            front.setTarget(binding.ivLuckPredictionCard)
+            back.setTarget(binding.ivLuckyCard)
+        }
+
+        front.doOnStart {
+            if(show){
+                binding.ivLuckPredictionCard.visibility = VISIBLE
+
+            } else {
                 binding.ivLuckyCard.visibility = VISIBLE
             }
 
-            override fun onAnimationEnd(animation: Animation?) {
-                growCard()
-            }
-        })
+            back.start()
+            binding.tvLuckPrediction.fade(show)
+            binding.cardLuckShare.fade(show)
+            binding.cardLuckTryAgain.fade(show)
+        }
 
-        binding.ivLuckyCard.startAnimation(slideUp)
+        front.doOnEnd {
+            if(show){
+                binding.ivLuckyCard.visibility = INVISIBLE
+                itsAnimated = false
+                isPredictionShowing = true
+
+            } else {
+                binding.ivLuckPredictionCard.visibility = INVISIBLE
+                scaleCard(false)
+            }
+        }
+
+        front.start()
     }
 
-    private fun growCard() {
-        val grow = AnimationUtils.loadAnimation(context, R.anim.grow)
-
-        grow.setAnimationListener(object : AnimationListener {
-            override fun onAnimationRepeat(animation: Animation?) {}
-            override fun onAnimationStart(animation: Animation?) {}
-
-            override fun onAnimationEnd(animation: Animation?) {
-                showLuckPrediction()
-            }
-        })
-
-        binding.ivLuckyCard.startAnimation(grow)
-    }
-
-    private fun showLuckPrediction() {
-        val showAnimation = AlphaAnimation(0f, 1f)
-        showAnimation.duration = 1000
-
-        val hideAnimation = AlphaAnimation(1f, 0f)
-        hideAnimation.duration = 200
-        hideAnimation.setAnimationListener(object : AnimationListener{
+    private fun View.fade(show:Boolean){
+        val fade = AnimationUtils.loadAnimation(context, if(show) fade_in else fade_out)
+        fade.setAnimationListener(object : AnimationListener{
             override fun onAnimationRepeat(animation: Animation?) {}
 
             override fun onAnimationStart(animation: Animation?) {
-                binding.layoutLuckPrediction.isVisible = true
-                binding.layoutLuckPrediction.startAnimation(showAnimation)
+                if(show) this@fade.visibility = VISIBLE
             }
             override fun onAnimationEnd(animation: Animation?) {
-                binding.layoutLuckyRoulette.isVisible = false
-                binding.ivLuckyCard.visibility = INVISIBLE
+                if(!show) this@fade.visibility = INVISIBLE
             }
         })
-
-        binding.layoutLuckyRoulette.startAnimation(hideAnimation)
-
+        this.startAnimation(fade)
     }
 
 }
--- app/src/main/java/com/example/horoscapp/ui/luck/provider/RandomCardProvider.kt
+++ app/src/main/java/com/example/horoscapp/ui/luck/provider/RandomCardProvider.kt
@@ -1,11 +1,43 @@
 package com.example.horoscapp.ui.luck.provider
 
+import com.example.horoscapp.R.drawable.*
+import com.example.horoscapp.R.string.*
+import com.example.horoscapp.ui.luck.model.LuckyModel
 import javax.inject.Inject
+import kotlin.random.Random
 
 class RandomCardProvider @Inject constructor() {
 
-    fun getLucky(){
-
+    fun getLucky():LuckyModel?{
+        return when(Random.nextInt(0,  26)){
+            0 -> LuckyModel(card_0, luck_0)
+            1 -> LuckyModel(card_1, luck_1)
+            2 -> LuckyModel(card_2, luck_2)
+            3 -> LuckyModel(card_3, luck_3)
+            4 -> LuckyModel(card_4, luck_4)
+            5 -> LuckyModel(card_5, luck_5)
+            6 -> LuckyModel(card_6, luck_6)
+            7 -> LuckyModel(card_7, luck_7)
+            8 -> LuckyModel(card_8, luck_8)
+            9 -> LuckyModel(card_9, luck_9)
+            10 -> LuckyModel(card_10, luck_10)
+            11 -> LuckyModel(card_11, luck_11)
+            12 -> LuckyModel(card_12, luck_12)
+            13 -> LuckyModel(card_13, luck_13)
+            14 -> LuckyModel(card_14, luck_14)
+            15 -> LuckyModel(card_15, luck_15)
+            16 -> LuckyModel(card_16, luck_16)
+            17 -> LuckyModel(card_17, luck_17)
+            18 -> LuckyModel(card_18, luck_18)
+            19 -> LuckyModel(card_19, luck_19)
+            20 -> LuckyModel(card_20, luck_20)
+            21 -> LuckyModel(card_21, luck_21)
+            22 -> LuckyModel(card_22, luck_22)
+            23 -> LuckyModel(card_23, luck_23)
+            24 -> LuckyModel(card_24, luck_24)
+            25 -> LuckyModel(card_25, luck_25)
+            else -> null
+        }
     }
 
 }
